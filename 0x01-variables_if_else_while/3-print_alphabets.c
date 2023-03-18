43bad646158a67e3507d39494356bec4e1d4e8be
Arabella0721 <ibukunoluwafakunle@gmail.com>
2023-03-17 19:42:47 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,18 @@
 
 int main(void)
 {
-	int lowerCase - 'a';
-	int upperCase - 'A';
+	int lowerCase = 'a';
+	int upperCase = 'A';
+
 	while )lowerCase <= 'z')
 	{
 		putchar(lowerCase);
 		lowerCase += 1;
 	}
 	while (upperCase <= 'z')
-	}
+	{
 	putchar(upperCase);
-	upperCase += 1;
+	uppercase += 1;
 	}
 	putchar('\n');
 	return (0);
