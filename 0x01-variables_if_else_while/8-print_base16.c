b7811ede988ad6958fde98fa5059543a429d32f3
Arabella0721 <ibukunoluwafakunle@gmail.com>
2023-03-18 07:37:03 -0700

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 	int i;
-	char j;
+	int j;
 
 	for (i = 0 ; i < 10 ; i++)
-		putchar(i + '0');
+	putchar(i + '0');
 	for (j = 'a' ; j <= 'f' ; j++)
-	       putchar(j);
+	putchar(j);
 	putchar('\n');
 	return (0);
 }
