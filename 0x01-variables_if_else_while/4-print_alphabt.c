475059d5c659f70a653134c8d2c381b5f28169de
Arabella0721 <ibukunoluwafakunle@gmail.com>
2023-03-17 21:32:25 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,18 +8,12 @@
 
 int main(void)
 {
-	int lowercase = 'a';
+	char i;
 
-	while (lowercase <= 'z')
-	{
-	if (lowercase == 'e' || lowercase  == 'q')
-	lowercase =+ 1;
-	}
-	else
-	{
-	putchar(lowercase);
-	lowercase =+ 1;
-	}
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
