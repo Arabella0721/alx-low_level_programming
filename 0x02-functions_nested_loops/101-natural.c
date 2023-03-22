6cb523aee504b4d535fc51aa172f24c88fcc3f53
Arabella0721 <ibukunoluwafakunle@gmail.com>
2023-03-22 14:26:26 -0700

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Print the sum of all multiples of 3 and 5 up to 1024
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,34 +1,26 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer
+ * jack_bauer - This function prints 24 hours
+ *
  * Return: 0 (success)
  */
 
 void jack_bauer(void)
-
 {
-	int a, b, c, d;
+	int hr;
+	int min;
 
-	for (a =0; a <= 2; a++)
+	for (hr = 0; hr < 24; hr++)
 	{
-		for (b =0; b <= 9; b++)
+		for (min = 0; min < 60; min++)
 		{
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-			{
-				for (c = 0; c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(58);
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar(hr / 10 + '0');
+			_putchar(hr % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
