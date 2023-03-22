ede5711881244af8a00ab2004466177d1ca628ff
Arabella0721 <ibukunoluwafakunle@gmail.com>
2023-03-22 13:49:55 -0700

    103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 1 and 2, separated bt a comma followed by a space
- * Return: 0 (success)
+ * main - Prints the sum of even fibonacci numbers
+ * less than 4000000
+ * Return: void
  */
 
 int main(void)
 
 {
-	int i;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+	int i = 0;
+	long j = 1, k = 2, sum = k;
 
-	for (i = 0; i < 92; i++)
+	while (k + j < 4000000)
 	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-		fib1 = fib2;
-		fib2 =sum;
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+		++i;
 	}
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-	for (i =93
+	printf("%ld\n", sum);
+	return (0);
+}
