815e0eab16fb6129b7c861649da452b59c7f8f44
Arabella0721 <ibukunoluwafakunle@gmail.com>
2023-03-22 16:25:18 -0700

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -27,7 +27,7 @@ int main(void)
 	fib2_half2 = fib2 % 10000000000;
 	for (i = 93; i < 99; i++)
 	{
-		half1 = fib1_half1 + fib2_half2;
+		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
