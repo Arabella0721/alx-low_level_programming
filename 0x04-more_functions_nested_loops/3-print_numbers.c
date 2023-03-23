7ee878e5ce9aad850bfa3467f95e7e701ca7b2b1
Arabella0721 <ibukunoluwafakunle@gmail.com>
2023-03-23 13:01:56 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putcha(r(i % 10) + '0');
+	}
+	_putchar("\n");
+}
