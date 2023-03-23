4561b656c07ebb6700db0082d80136c1814e8738
Arabella0721 <ibukunoluwafakunle@gmail.com>
2023-03-23 12:33:49 -0700

    1-isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,16 +2,19 @@
 
 /**
  * _isdigit - Check for a digit 0 through 9
- *
+ * @c: The character to be checked
  * Return: 1 if c is a digit and 0 if otherwise
  */
 
 int _isdigit(int c)
 {
 
-	if (c >= 48 && c <= 57)
+	if (c >= '48' && c <= '57')
 	{
 		return (1);
 	}
+	else
+	{
 	return (0);
+	}
 }
