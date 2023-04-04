9f82706359ec1b77b3acd4ed8a1ff480936779cf
Arabella0721 <ibukunoluwafakunle@gmail.com>
2023-04-03 17:39:56 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 
 /**
   * _memset - fills memory with a constant byte
-  * @*s: memory area to be filled
+  * @s: memory area to be filled
   * @b: char to copy
   * @n: number of times b would be copied
   * Return: pointer to s
